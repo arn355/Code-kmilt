41da848d30bada7f76ca7e424b2fe54a1c30442d
arn355 <86312780+arn355@users.noreply.github.com>
2021-08-18 20:51:36 +0700

    Add files via upload

---
+++ Problem1v1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+float a,b;
+
+main(){
+    printf("First number : ");
+    scanf("%f",&a);
+    printf("Second number : ");
+    scanf("%f",&b);
+    printf("Add : %f\n",a+b);
+    printf("Subtract : %f\n",a-b);
+    printf("Multiply : %f\n",a*b);
+    printf("Divide : %f",a/b);
+    return 0;
+}
+
+
