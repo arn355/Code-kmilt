a4194f7725209297b988c1d2e2064fb74e9978a7
arn355 <86312780+arn355@users.noreply.github.com>
2021-08-25 21:52:00 +0700

    Add files via upload

---
+++ Problem2v1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    float num1, num2, num3;
+    printf("Enter three numbers: ");
+    scanf("%f %f %f", &num1, &num2, &num3);
+
+    if(num1 >= num2){
+        if(num1 >= num3){
+        	printf("%.2f is the largest number.", num1);
+		}else{
+			printf("%.2f is the largest number.", num3);
+		}            
+    }else{
+        if(num2 >= num3){
+        	printf("%.2f is the largest number.", num2);
+		}    
+        else{
+        	printf("%.2f is the largest number.", num3);
+		}      
+    }
+    return 0;
+}
